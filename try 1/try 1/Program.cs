ce98eb3466155d715d1970eb4e6b8cacd3c9098b
KKaviya1 <139097458+KKaviya1@users.noreply.github.com>
2023-07-12 11:07:28 +0530

    Add files via upload

---
+++ try 1/try 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a number");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int result))
+                {
+                    Console.WriteLine("Parsed number using intTryParse is: \t " + result);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to Parse");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
